f1c8a72d74e542f8daec2a4e34b88eff61015d36
Babar78 <muhammadbabarashraf8@gmail.com>
2025-09-10 23:50:46 +0500

    feat: added_sectionDivider

--- src/app/(landing-page)/page.tsx
+++ src/app/(landing-page)/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar/Navbar";
 import HeroSection from "@/modules/landing/components/HeroSection";
+import SectionDivider from "@/modules/landing/components/SectionDivider";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900 text-white overflow-hidden">
       <Navbar />
       <HeroSection />
+      <SectionDivider />
     </div>
   );
 }
