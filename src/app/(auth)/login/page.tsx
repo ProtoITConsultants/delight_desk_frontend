cfe42b1a2b875ff227c7c22b745a753347e39bf3
Babar78 <muhammadbabarashraf8@gmail.com>
2025-09-17 00:27:21 +0500

    feat: create_login_page

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,58 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import LoginForm from "@/modules/auth/components/LoginForm";
+import Link from "next/link";
+
+const LoginPage = () => {
+  return (
+    <>
+      {/* Header */}
+      <div className="text-center">
+        <h1 className="text-3xl font-bold text-gray-900">Welcome Back</h1>
+        <p className="mt-2 text-sm text-gray-600">
+          Sign in to access your customer service dashboard
+        </p>
+      </div>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-xl text-center">Sign In</CardTitle>
+          <CardDescription className="text-center">
+            Enter your credentials to continue
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <LoginForm />
+          <div className="mt-4 text-center">
+            <Link
+              href={"/forgot-password"}
+              prefetch
+              className="text-sm text-blue-600 hover:text-blue-500 font-medium"
+            >
+              Forgot your password?
+            </Link>
+          </div>
+
+          <div className="mt-6 text-center">
+            <p className="text-sm text-gray-600">
+              Don&apos;t have an account?{" "}
+              <Link
+                href="/signup"
+                prefetch
+                className="text-blue-600 hover:text-blue-500 font-medium"
+              >
+                Sign up
+              </Link>
+            </p>
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
+
+export default LoginPage;
