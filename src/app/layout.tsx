c9301538ad8edbbeb6aec15b79c4abe368ae8c7f
babar78 <muhammadbabarashraf8@gmail.com>
2025-09-27 00:07:42 +0500

    feat: add_tanstack_query_client_for_data_cache

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
+import QueryProvider from "@/providers/query-provider";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -31,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} antialiased`}>
-        {children}
+        <QueryProvider>{children}</QueryProvider>
         <Toaster />
       </body>
     </html>
