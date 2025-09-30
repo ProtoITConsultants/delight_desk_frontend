a001f5e9a0168c17fed5678f7c72625f232f0777
babar78 <muhammadbabarashraf8@gmail.com>
2025-09-30 23:43:14 +0500

    feat: create_layout_for_pages_without_sidebar

---
+++ src/app/(no-sidebar-pages)/layout.tsx
@@ -0,0 +1,21 @@
+import GenericDashboardNavbar from "@/modules/core/components/generic-dashboard-navbar";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard - Delight Desk",
+};
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <GenericDashboardNavbar />
+      <main className="flex-1 relative overflow-y-auto focus:outline-none">
+        {children}
+      </main>
+    </div>
+  );
+}
