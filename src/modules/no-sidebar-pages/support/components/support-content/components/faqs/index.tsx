800d6b9d1fbfd43738f2ff41a49a69e460209fe4
babar78 <muhammadbabarashraf8@gmail.com>
2025-10-01 01:50:30 +0500

    feat: created_a_centeralized_contact_us_form

--- src/modules/no-sidebar-pages/support/components/support-content/components/faqs/index.tsx
+++ src/modules/no-sidebar-pages/support/components/support-content/components/faqs/index.tsx
@@ -1,8 +1,16 @@
 "use client";
+import { Mail } from "lucide-react";
 import FAQsContent from "./components/accordion-content";
 import FAQsSidebar from "./components/accordion-sidebar";
-import ContactUsForm from "./components/contact-us-form";
+import ContactUsForm from "@/modules/core/components/contact-us-form";
 import { FAQSidebarProvider } from "./context/use-faq-sidebar";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const FAQs = () => {
   return (
@@ -12,8 +20,25 @@ const FAQs = () => {
           <FAQsSidebar />
         </div>
         <div className="col-span-1 lg:col-span-3" id="faq-section">
+          {/* FAQs Content - Accordions */}
           <FAQsContent />
-          <ContactUsForm />
+
+          {/* Contact Us Form */}
+          <Card className="mt-8" id="contact-form">
+            <CardHeader>
+              <CardTitle className="flex items-center gap-2 text-2xl">
+                <Mail className="h-5 w-5" />
+                Still Need Help?
+              </CardTitle>
+              <CardDescription>
+                Can&apos;t find what you&apos;re looking for? Send us a message
+                and we&apos;ll get back to you within 24 hours.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <ContactUsForm formType="get-help" />
+            </CardContent>
+          </Card>
         </div>
       </div>
     </FAQSidebarProvider>
