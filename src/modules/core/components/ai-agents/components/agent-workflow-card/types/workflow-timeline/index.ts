32dc47aa212d7a366a34edcb03166afecf6cc287
babar78 <muhammadbabarashraf8@gmail.com>
2025-10-11 01:23:28 +0500

    fix: fixed_fulfillment_method_types_import

--- src/modules/core/components/ai-agents/components/agent-workflow-card/types/workflow-timeline/index.ts
+++ src/modules/core/components/ai-agents/components/agent-workflow-card/types/workflow-timeline/index.ts
@@ -1,8 +1,9 @@
-import { FULLFILLMENT_METHODS, WORKFLOW_STATUS_TYPE } from "../index";
+import { FULLFILLMENT_METHODS_TYPES } from "@/modules/core/utils/order-fulfillment-methods/types";
+import { WORKFLOW_STATUS_TYPE } from "../index";
 
 type WORKFLOW_TIMELINE_PROPS = {
   workflowId: string;
-  fulfillmentMethod: FULLFILLMENT_METHODS;
+  fulfillmentMethod: FULLFILLMENT_METHODS_TYPES;
   workflowStatus: WORKFLOW_STATUS_TYPE;
 };
 
