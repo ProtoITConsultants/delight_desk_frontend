beffe191cdf4ce4c786aaa2d172cfd73d8c64b2d
babar78 <muhammadbabarashraf8@gmail.com>
2025-10-14 02:17:14 +0500

    feat: add_some_types_related_to_the_pending_action_tab

--- src/modules/protected-routes/approval-queue/components/approval-queue-tabs/types/pending-action-tabs/index.ts
+++ src/modules/protected-routes/approval-queue/components/approval-queue-tabs/types/pending-action-tabs/index.ts
@@ -1,3 +1,23 @@
+// Data Structure for Pending Action Items (Fetched From Backend)
+type PLANNED_ACTION_TYPE = {
+  actionName: string;
+  actionDetails: string;
+};
+
+type PENDING_ACTION_ITEM_TYPE = {
+  id: string;
+  emailSubject: string;
+  emailBody: string;
+  customerEmail: string;
+  emailReceivedAt: string;
+  requestType: string;
+  proposedAIResponse: string;
+  plannedActions: PLANNED_ACTION_TYPE[];
+  // Extra Details related to the pending action
+  pending_action_details: string;
+};
+
+// Types of Filter Tabs Shown In Pending Actions Tab
 type PENDING_ACTIONS_FILTERS_TYPES =
   | "all"
   | "returns-agent"
@@ -9,13 +29,15 @@ type PENDING_ACTIONS_FILTERS_TYPES =
   | "product-agent"
   | "promo-code-agent";
 
+// Structure of Each Filter Tab Item
 interface PENDING_ACTION_FILTER_TAB_ITEM {
   id: string;
   type: PENDING_ACTIONS_FILTERS_TYPES;
   label: string;
-  items: [];
+  items: PENDING_ACTION_ITEM_TYPE[];
 }
 
+// Structure of Filter Tabs Array
 type PENDING_ACTION_FILTER_TABS = PENDING_ACTION_FILTER_TAB_ITEM[];
 
 export type {
