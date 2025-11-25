37332e87e16ea5829c8aefeee8208f05ce632c12
babar78 <muhammadbabarashraf8@gmail.com>
2025-11-26 00:50:06 +0500

    feat: implement admin authentication service and response conversion

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -4,6 +4,10 @@ import {
   convertAuthenticateUserResponse,
 } from "./utils/user-auth";
 import AUTHENTICATION_ENDPOINTS from "./constants";
+import {
+  AUTHENTICATE_ADMIN_RESPONSE,
+  convertAuthenticateAdminResponse,
+} from "./utils/admin-auth";
 
 export class UserAuthenticationService {
   // Authenticate user
@@ -17,5 +21,13 @@ export class UserAuthenticationService {
     return dto;
   };
   // Authenticate admin
-  authenticateAdmin = async () => {};
+  authenticateAdmin = async () => {
+    const response = await apiService.get<AUTHENTICATE_ADMIN_RESPONSE>(
+      AUTHENTICATION_ENDPOINTS.AUTHENTICATE_ADMIN
+    );
+
+    const dto = convertAuthenticateAdminResponse(response);
+
+    return dto;
+  };
 }
