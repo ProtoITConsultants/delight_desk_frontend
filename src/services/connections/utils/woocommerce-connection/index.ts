4bbdd81e6fe39eb33a1eb6f89052e5f4c2bd12b4
babar78 <muhammadbabarashraf8@gmail.com>
2025-11-27 02:48:12 +0500

    feat: add WooCommerce connection URLs and request/response types

---
+++ src/services/connections/utils/woocommerce-connection/index.ts
@@ -0,0 +1,14 @@
+// Req Params Type
+type WOOCOMMERCE_CONNECTION_BASE_PARAMS = {
+  storeUrl: string;
+};
+
+export type WOOCOMMERCE_CONNECTION_THROUGH_OAUTH_PARAMS =
+  WOOCOMMERCE_CONNECTION_BASE_PARAMS;
+export type WOOCOMMERCE_CONNECTION_THROUGH_SECRET_KEYS_PARAMS =
+  WOOCOMMERCE_CONNECTION_BASE_PARAMS & {
+    consumerKey: string;
+    consumerSecret: string;
+  };
+
+// Response Types
